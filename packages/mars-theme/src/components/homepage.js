a3d9376c7bd09e223f1fd0128a637a32bac58f70
erica2605@live.com <erica2605@live.com>
2020-10-17 18:03:14 -0400

    welcome text added and formatted

--- packages/mars-theme/src/components/homepage.js
+++ packages/mars-theme/src/components/homepage.js
@@ -18,6 +18,11 @@ const Homepage = ({ state, libraries }) => {
         <WelcomeArea>
           <WelcomeText src={"http://5zq.7eb.myftpupload.com/wp-content/uploads/2020/10/Welcome-1.png"}/>
           <WelcomePhoto src={"http://5zq.7eb.myftpupload.com/wp-content/uploads/2020/10/IMG_8903-1-scaled.jpg"}/>
+          <WelcomeBio>Hi! My name is Demi Bang and welcome to my blog! I'm an Arizona blogger and influencer that focuses primarily on beauty, college/career, and travel. I'm a recent Arizona State University graduate and documenting my journey as I navigate through adulthood. In addition to my blog and social media, I work as a marketing expert at my day job and a nail technician on the weekends. <br></br><br></br><br></br>
+
+Aside from constantly working, I'm also a cat mom to a chunky Bombay cat named Henry. I love learning new things, working out, and hosting game nights with friends.
+<br></br><br></br><br></br>
+Don't forget to subscribe to my newsletter down below.</WelcomeBio>
           <WelcomeContainerFront/>
           <WelcomeContainerBack/>
         </WelcomeArea>
@@ -120,5 +125,14 @@ const LatestPostsContainer = styled.div`
   width: 100%;
   height: 50%;
   top: 180%;
+`;
+
+const WelcomeBio = styled.p`
+  width: 500px;
+  z-index: 100;
+  top: 100px;
+  position: absolute;
+  left: 350px;
+  font-size: 19px;
 `;
 
